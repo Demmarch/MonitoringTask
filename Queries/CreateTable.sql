e9ef4970ea638f531884e9e21762dec6f334d542
Demmarch <yar310306d@gmail.com>
2025-07-09 09:32:55 +0300

    Create table status_log

--- Queries/CreateTable.sql
+++ Queries/CreateTable.sql
@@ -6,3 +6,13 @@ CREATE TABLE devices
 	dev_description varchar(255),
 	dev_status int NOT NULL
 )
+
+CREATE TABLE status_log
+(
+    log_id SERIAL PRIMARY KEY,
+    device_id INT NOT NULL REFERENCES devices(id) ON DELETE CASCADE,
+    dev_num VARCHAR(255) NOT NULL,
+    change_time TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    old_status INT NOT NULL,
+    new_status INT NOT NULL
+);
